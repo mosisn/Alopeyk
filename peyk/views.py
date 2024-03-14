d7942431e4969af430a2848a1ddf2956a1b387c9
Mostafa <mostafa.saneisn@gmail.com>
2024-03-14 19:48:56 +0330

    Feat: create authorization using otp APIview

--- peyk/views.py
+++ peyk/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework import status, generics
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Account, Order
 from django.contrib.auth.hashers import make_password
 from .serializers import AccountSerializer, OrderSerrializer
+from .authentication import create_access_token, create_refresh_token
 from random import randint
+import traceback
 
 
 
@@ -26,8 +29,41 @@ class AccountList(APIView):
                 user.set_password(password)
                 user.save()
             
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({"otp": otp}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FirstLoginView(APIView):
+    def post(self, request):
+        body = request.data
+        phone_number = body.get('phone_number')
+        otp = body.get('otp')
+        
+        try:
+            user = Account.objects.get(phone_number=phone_number)
+            if user.is_authorized == False:
+                if user.otp == otp:
+                    user.otp = 0
+                    user.is_authorized = True
+                    user.save()
+                    
+                    current_user = Account.objects.get(phone_number=phone_number)
+                    access_token = create_access_token(current_user.id)
+                    refresh_token = create_refresh_token(current_user.id)
+                    
+                    res = Response(access_token)
+                    res.set_cookie('refresh_token', refresh_token, httponly=True)
+                    
+                    return res
+                else:
+                    return Response("Otp is Wrong!", status=status.HTTP_403_FORBIDDEN)
+            else:
+                return Response("User is already authorized")
+        except Account.DoesNotExist:
+            return Response("User not found with the provided phone number", status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            traceback.print_exc()
+            return Response("An error occurred", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class OrderList(generics.ListCreateAPIView):
     queryset = Order.objects.all()
