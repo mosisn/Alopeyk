5fda9a68d16fc23b5a5f860706c47b64a1c2130c
Mostafa <mostafa.saneisn@gmail.com>
2024-03-14 19:50:42 +0330

    update: url for authentication view

--- peyk/urls.py
+++ peyk/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import AccountList, OrderList
+from .views import AccountList, OrderList, FirstLoginView
+
+
 urlpatterns = [
     path('register/', AccountList.as_view(), name='register'),
+    path('register/auth', FirstLoginView.as_view(), name='first time login'),
     path('order/',OrderList.as_view(), name='order list' )
 ]
